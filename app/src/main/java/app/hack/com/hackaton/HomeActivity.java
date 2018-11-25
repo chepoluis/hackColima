377fcf0280e81cb360b3fc458387c0548a2857e5
Luis <vsjoseluisvs@gmail.com>
2018-11-25 12:48:13 -0600

    Se agrego la funcionalidad de mostrar los datos en el activity principal

--- app/src/main/java/app/hack/com/hackaton/HomeActivity.java
+++ app/src/main/java/app/hack/com/hackaton/HomeActivity.java
@@ -5,18 +5,51 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import app.hack.com.hackaton.Model.Reports;
 
 public class HomeActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mUserDatabase;
+    private String userID;
+
+    TextView empty;
+    ImageView icon_empty;
+
+    private RecyclerView mPeopleRV;
+    private DatabaseReference mDatabase;
+    private FirebaseRecyclerAdapter<Reports, ReportsActivity.ProductsViewHolder> mProductRVAdapter;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -24,13 +57,10 @@ public class HomeActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
             return false;
@@ -48,6 +78,78 @@ public class HomeActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(toolbar);
+
+        mAuth = FirebaseAuth.getInstance();
+        userID = mAuth.getCurrentUser().getUid();
+        mUserDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(userID);
+
+        empty = findViewById(R.id.empty);
+        icon_empty = findViewById(R.id.icon_empty);
+
+        //"Products" here will reflect what you have called your database in Firebase.
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Report");
+        mDatabase.keepSynced(true);
+
+        // Check if there are products and show a text
+        final DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+        rootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                // If Products child exist
+                if (snapshot.hasChild("Report")) {
+                    empty.setVisibility(View.GONE);
+                    icon_empty.setVisibility(View.GONE);
+                } else {
+                    empty.setVisibility(View.VISIBLE);
+                    icon_empty.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.e("Error: ", databaseError.toString());
+            }
+        });
+
+        mPeopleRV = (RecyclerView) findViewById(R.id.myRecycleView);
+
+        DatabaseReference personsRef = FirebaseDatabase.getInstance().getReference().child("Report");
+        Query personsQuery = personsRef.orderByKey();
+
+        mPeopleRV.hasFixedSize();
+        mPeopleRV.setLayoutManager(new LinearLayoutManager(this));
+
+        FirebaseRecyclerOptions personsOptions = new FirebaseRecyclerOptions.Builder<Reports>()
+                .setQuery(personsQuery, Reports.class).build();
+
+        mProductRVAdapter = new FirebaseRecyclerAdapter<Reports, ReportsActivity.ProductsViewHolder>(personsOptions) {
+            @Override
+            protected void onBindViewHolder(@NonNull ReportsActivity.ProductsViewHolder holder, int position, @NonNull Reports reports) {
+                holder.setTitle(reports.getTitle());
+                holder.setDescription(reports.getDescription());
+                holder.setDate("Estado: " + reports.getStatus());
+                holder.setImage(getBaseContext(), reports.getPicture());
+
+
+                holder.mView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(HomeActivity.this, "Hack Colima 2018", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public ReportsActivity.ProductsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+                View view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.view_report, parent, false);
+
+                return new ReportsActivity.ProductsViewHolder(view);
+            }
+        };
+
+        mPeopleRV.setAdapter(mProductRVAdapter);
     }
 
     @Override
@@ -69,5 +171,17 @@ public class HomeActivity extends AppCompatActivity {
                 break;
         }
         return true;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mProductRVAdapter.startListening();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mProductRVAdapter.stopListening();
     }
 }
